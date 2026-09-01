52ee5a2393e84ea35e151ae007bd399a6620ffd1
javakishore-veleti <6062900+javakishore-veleti@users.noreply.github.com>
2026-09-01 19:07:25 -0400

    Add SQL question curation and a Postgres helper for schema and query execution.

--- Models/schema.py
+++ Models/schema.py
@@ -2,12 +2,13 @@ from pydantic import BaseModel, Field
 from typing import Annotated, Literal
 from operator import add
 
-class AgentState(BaseModel):
+class AgentSchema(BaseModel):
     """
     Represents the state of an agent.
     """
     messages:Annotated[list,add] = Field(..., description="The messages of the agent." )
-    curated_ques : str = Field(..., description="The curated question of the agent." )
+    user_question: str = Field(..., description="The user question of the agent." )
+    curated_question : str = Field(..., description="The curated question of the agent." )
     prompt_query_context: str = Field(..., description="The prompt query context of the agent." )
     is_safe: Literal["Yes", "No"] = Field(..., description="Whether the agent is safe." )
     generated_sql_query: str = Field(..., description="The generated SQL query of the agent." )
--- agents/sql_analyst.py
+++ agents/sql_analyst.py
@@ -1,1 +1,24 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from Models.schema import AgentSchema
 from utils.llm_pickup import pick_llm
+
+
+def curate_question(state: AgentSchema) -> AgentSchema:
+
+    user_question = state.user_question
+
+    llm = pick_llm("low")
+    response = llm.invoke(f"Curate the following question: {user_question}")
+    curated_question = response.text
+    state.curated_question = curated_question
+
+    return state
+
+def prompt_query_context(state: AgentSchema) -> AgentSchema:
+    curated_question = state.curated_question
+
+    return state
+
+
